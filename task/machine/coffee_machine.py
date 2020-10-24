76941c4b25aaeb44abc18bc6e6ee61c109205d39
MikalaiK <35535732+mikalaikarol@users.noreply.github.com>
2020-10-24 13:40:29 +0200

    after refactoring

--- task/machine/coffee_machine.py
+++ task/machine/coffee_machine.py
@@ -2,17 +2,11 @@
 
 # global variables
 
-water_storage = 400
-milk_storage = 540
-coffee_storage = 120
-cups_storage = 9
-money_storage = 550
-
+water_storage, milk_storage, coffee_storage, cups_storage, money_storage = 400, 540, 120, 9, 550
 program_is_running = True
 
 
 def show_left_ingredients():
-    print("")
     print("The coffee machine has: ")
     print(water_storage, "of water")
     print(milk_storage, "of milk")
@@ -22,14 +16,9 @@ def show_left_ingredients():
 
 
 def calculate_usage_espresso():
-    global water_storage
-    global coffee_storage
-    global money_storage
-    global cups_storage
-    water_usage = 250
-    coffee_usage = 16
-    cost = 4
-    cups_usage = 1
+    global water_storage, coffee_storage, money_storage, cups_storage
+    water_usage, coffee_usage, cost, cups_usage = 250, 16, 4, 1
+
     if water_storage < water_usage:
         print("Sorry, not enough water!")
     elif coffee_storage < coffee_usage:
@@ -38,27 +27,16 @@ def calculate_usage_espresso():
         print("Sorry, not enough cups!")
     else:
         print("I have enough resources, making you a coffee!")
-        water_storage = water_storage - water_usage
-        coffee_storage = coffee_storage - coffee_usage
-        money_storage = money_storage + cost
-        cups_storage = cups_storage - cups_usage
-
-
-def output_espresso():
-    calculate_usage_espresso()
+        water_storage -= water_usage
+        coffee_storage -= coffee_usage
+        money_storage += cost
+        cups_storage -= cups_usage
 
 
 def calculate_usage_latte():
-    global water_storage
-    global coffee_storage
-    global milk_storage
-    global cups_storage
-    global money_storage
-    water_usage = 350
-    milk_usage = 75
-    coffee_usage = 20
-    cost = 7
-    cups_usage = 1
+    global water_storage, coffee_storage, milk_storage, money_storage, cups_storage
+    water_usage, milk_usage, coffee_usage, cost, cups_usage = 350, 75, 20, 7, 1
+
     if water_storage < water_usage:
         print("Sorry, not enough water!")
     elif coffee_storage < coffee_usage:
@@ -69,28 +47,17 @@ def calculate_usage_latte():
         print("Sorry, not enough cups!")
     else:
         print("I have enough resources, making you a coffee!")
-        water_storage = water_storage - water_usage
-        coffee_storage = coffee_storage - coffee_usage
-        milk_storage = milk_storage - milk_usage
-        money_storage = money_storage + cost
-        cups_storage = cups_storage - cups_usage
-
-
-def output_latte():
-    calculate_usage_latte()
+        water_storage -= water_usage
+        coffee_storage -= coffee_usage
+        milk_storage -= milk_usage
+        money_storage += cost
+        cups_storage -= cups_usage
 
 
 def calculate_usage_cappuccino():
-    global water_storage
-    global coffee_storage
-    global milk_storage
-    global money_storage
-    global cups_storage
-    water_usage = 200
-    milk_usage = 100
-    coffee_usage = 12
-    cost = 6
-    cups_usage = 1
+    global water_storage, coffee_storage, milk_storage, money_storage, cups_storage
+    water_usage, milk_usage, coffee_usage, cost, cups_usage = 200, 100, 12, 6, 1
+
     if water_storage < water_usage:
         print("Sorry, not enough water!")
     elif coffee_storage < coffee_usage:
@@ -101,39 +68,27 @@ def calculate_usage_cappuccino():
         print("Sorry, not enough cups!")
     else:
         print("I have enough resources, making you a coffee!")
-        water_storage = water_storage - water_usage
-        coffee_storage = coffee_storage - coffee_usage
-        milk_storage = milk_storage - milk_usage
-        money_storage = money_storage + cost
-        cups_storage = cups_storage - cups_usage
-
-
-def output_cappuccino():
-    calculate_usage_cappuccino()
+        water_storage -= water_usage
+        coffee_storage -= coffee_usage
+        milk_storage -= milk_usage
+        money_storage += cost
+        cups_storage -= cups_usage
 
 
 def buy_coffee():
-    print("")
     choice = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
-    if str(choice) == "back":
+    if choice == "back":
         return
-    if int(choice) == 1:
-        output_espresso()
-
-    if int(choice) == 2:
-        output_latte()
-
-    if int(choice) == 3:
-        output_cappuccino()
+    if choice == "1":
+        calculate_usage_espresso()
+    if choice == "2":
+        calculate_usage_latte()
+    if choice == "3":
+        calculate_usage_cappuccino()
 
 
 def fill_ingredients():
-    global water_storage
-    global coffee_storage
-    global milk_storage
-    global money_storage
-    global cups_storage
-    print("")
+    global water_storage, coffee_storage, milk_storage, money_storage, cups_storage
     add_water = int(input("Write how many ml of water do you want to add: "))
     add_milk = int(input("Write how many ml of milk do you want to add: "))
     add_coffee = int(input("Write how many grams of coffee beans do you want to add: "))
@@ -146,15 +101,12 @@ def fill_ingredients():
 
 def take_money():
     global money_storage
-    print("")
     print("I gave you $" + str(money_storage))
     money_storage = 0
 
 
 while program_is_running is True:
-    print("")
     option = input("Write action (buy, fill, take, remaining, exit): ")
-
     if option == "buy":
         buy_coffee()
     elif option == "fill":
